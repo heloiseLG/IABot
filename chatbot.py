83b1d8ae61a9bff6d98b1dcd80dee01c796125ff
emma-loise <emma-loise.quin@epitech.digital>
2024-02-02 16:19:13 +0100

    importer env

--- chatbot.py
+++ chatbot.py
@@ -1,17 +1,22 @@
 from langchain.agents.agent_types import AgentType
 from typing_extensions import Required, NotRequired
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
+from langchain_openai import OpenAI
 from langchain_openai import ChatOpenAI
 from langchain_experimental.agents import create_pandas_dataframe_agent
 import pandas as pd
+from dotenv import load_dotenv
+
 import os
 
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+load_dotenv()
+
+openai_api_key = os.environ.get("OPENAI_API_KEY")
 
 df = pd.read_json("Data.json")
 
 agent = create_pandas_dataframe_agent(
-    ChatOpenAI(openai_api_key = OPENAI_API_KEY, temperature=0, model="gpt-3.5-turbo-0613"),
+    ChatOpenAI(openai_api_key = openai_api_key, temperature=0, model="gpt-3.5-turbo-0613"),
     df,
     verbose=True,
     agent_type=AgentType.OPENAI_FUNCTIONS,

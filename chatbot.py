220d2b705724f043095e1ffbc826363f8f2541f2
heloiseLG <hlegeldron@gmail.com>
2024-02-02 09:38:17 +0100

    echange avec chatbot

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+from langchain.agents.agent_types import AgentType
+from typing_extensions import Required, NotRequired
+from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
+from langchain_openai import OpenAI
+from langchain_openai import ChatOpenAI
+from langchain_experimental.agents import create_pandas_dataframe_agent
+import pandas as pd
+
+openai_api_key = "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"
+
+df = pd.read_json("bon.json")
+
+agent = create_pandas_dataframe_agent(
+    ChatOpenAI(openai_api_key = openai_api_key, temperature=0, model="gpt-3.5-turbo-0613"),
+    df,
+    verbose=True,
+    agent_type=AgentType.OPENAI_FUNCTIONS,
+)
+
+agent.run("Quel est le deuxième Titre ? et quelle est sa Date de découverte ?")
---
+++ scrapeJSON.py
@@ -0,0 +1,71 @@
+from bs4 import BeautifulSoup as bs
+import requests
+from tabulate import tabulate
+import json
+from itertools import zip_longest
+
+def get_titles_and_hrefs_from_page(url):
+    response = requests.get(url)
+    html = response.content
+    soup = bs(html, "html.parser")
+
+    titres_oeuvres = soup.find_all("h3", class_="card__title ellipsis")
+    liens_oeuvres = soup.find_all("a", class_="h_4")
+
+    oeuvres_info = [
+        {
+            'titre': titre.get_text(strip=True),
+            'href': lien.get('href')
+        }
+        for titre, lien in zip(titres_oeuvres, liens_oeuvres)
+    ]
+
+    return oeuvres_info
+
+def get_infos_oeuvre(lien):
+    response = requests.get(lien)
+    html = response.content
+    soup = bs(html, "html.parser")
+
+    titre_oeuvre = soup.find_all("h1", class_="notice__title h_1")
+    descriptions_oeuvre = soup.find_all("div", class_="m-7col m-last part__content")
+
+    # Utiliser une liste pour stocker toutes les descriptions
+    descriptions = [desc.get_text(strip=True) for desc in descriptions_oeuvre]
+
+    info = [
+        {
+            'Titre': titre.get_text(strip=True),
+            "Descriptions de l'oeuvre": descriptions,
+        }
+        for titre in titre_oeuvre
+    ]
+
+    return info
+
+
+def loop_page():
+    base_url = "https://collections.louvre.fr/recherche?page="
+    num_pages = 2
+
+    for page_num in range(1, num_pages + 1):
+        url = base_url + str(page_num)
+        oeuvres_info = get_titles_and_hrefs_from_page(url)
+        print(f"Page {page_num} - Œuvres :\n{tabulate(oeuvres_info, headers='keys', tablefmt='pretty')}")
+
+def loop_lien(liens_oeuvres):
+    href_url = "https://collections.louvre.fr"
+    all_data = []
+
+    for oeuvre in liens_oeuvres:
+        lien = href_url + oeuvre['href']
+        info = get_infos_oeuvre(lien)
+        all_data.extend(info)
+        
+    print(all_data)
+    with open('mo.json', 'w', encoding='utf-8') as json_file:
+        json.dump(all_data, json_file, ensure_ascii=False, indent=4)
+
+liens_oeuvres = get_titles_and_hrefs_from_page("https://collections.louvre.fr/recherche?page=1")
+
+loop_lien(liens_oeuvres)

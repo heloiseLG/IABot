80257a768b173bdd61749b3bbaf2408b1670a4fe
heloiseLG <hlegeldron@gmail.com>
2024-02-02 09:52:21 +0100

    data

--- chatbot.py
+++ chatbot.py
@@ -9,7 +9,7 @@ import pandas as pd
 df = pd.read_json("Data.json")
 
 agent = create_pandas_dataframe_agent(
-    ChatOpenAI(openai_api_key = openai_api_key, temperature=0, model="gpt-3.5-turbo-0613"),
+    ChatOpenAI(openai_api_key = key, temperature=0, model="gpt-3.5-turbo-0613"),
     df,
     verbose=True,
     agent_type=AgentType.OPENAI_FUNCTIONS,
